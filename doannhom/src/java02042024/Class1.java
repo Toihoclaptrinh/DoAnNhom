13db3ab1fdf1ab01eb9d430249737d7617a82b2e
thaiv <thaiv@MSI>
2024-04-03 00:20:24 +0700

    commit document by thai vinh

--- doannhom/src/java02042024/Class1.java
+++ doannhom/src/java02042024/Class1.java
@@ -3,6 +3,7 @@ package java02042024;
 public class Class1 {
 	    public void method1() {
 	        System.out.println("This is method1 of Class1 from Thai Vinh");
+	        System.out.println("update");
 	    }
 	}
 
