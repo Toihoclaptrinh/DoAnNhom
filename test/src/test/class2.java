5c090ce8889758db3185863b1b17ba5f0cd9c6aa
ADMIN <ADMIN@nmb>
2024-04-03 14:52:26 +0700

    version 2

--- test/src/test/class2.java
+++ test/src/test/class2.java
@@ -2,6 +2,6 @@ package test;
 
 public class class2 {
 	public void method2() {
-		System.out.println("This is method1 of Class1 from Binh Minh");
+		System.out.println("This is method1 of Class1 from Binh Minhh");
 	}
 }
